316673b46a319696b7fc5d7fd9e08db035509892
William Sandvej Hansen <grusinator@gmail.com>
2018-07-10 09:15:07 +0200

    Add project files.

---
+++ PersonalDataApi/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\William S. Hansen\source\repos\web\PersonalDataApi\PersonalDataApi\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
